049e74f8e93fd5e5f48396ded1a4d27874a65a63
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-12-27 10:38:49 +0700

    Initial commit

---
+++ lib/Tie/Array/NoOp.pm
@@ -0,0 +1,74 @@
+## no critic: TestingAndDebugging::RequireUseStrict
+package Tie::Scalar::NoOp;
+
+# IFUNBUILT
+use strict;
+# END IFUNBUILT
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+sub TIEARRAY {
+    my $class = shift;
+
+    bless [], $class;
+}
+
+sub FETCH {}
+
+sub STORE {}
+
+sub FETCHSIZE {1}
+
+sub STORESIZE {1}
+
+sub EXTEND {}
+
+sub EXISTS {}
+
+sub DELETE {}
+
+sub CLEAR {}
+
+sub PUSH {}
+
+sub POP {}
+
+sub SHIFT {}
+
+sub UNSHIFT {}
+
+sub SPLICE {}
+
+sub UNTIE {}
+
+# DESTROY
+
+1;
+# ABSTRACT: Tied array that does nothing
+
+=for Pod::Coverage ^(.+)$
+
+=head1 SYNOPSIS
+
+ use Tie::Array::NoOp;
+
+ tie my @ary, 'Tie::Array::NoOp';
+
+
+=head1 DESCRIPTION
+
+This class implements a tied array that does nothing. For benchmark purposes.
+
+
+=head1 SEE ALSO
+
+L<perltie>
+
+Other C<Tie::*::NoOp>
+
+L<Bencher::Scenarios::Tie>
+
+=cut
